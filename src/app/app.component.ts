cfe4b8bad8895efbc1ef131d732e6982e8f52660
Rolando Ramirez <7200ws@gmail.com>
2024-09-04 18:00:46 -0500

    todo for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from "./pages/todo/todo.component";
+import { TODO_DATA } from '../assets/todo';
+import { NTodo } from './models/todo.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoComponent],
+  imports: [RouterOutlet, CommonModule, TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'riadaui';
+  todoData = TODO_DATA;
+  getTodoInfo(val: NTodo.TodoData){
+    console.log(val);
+  }
+
+  trackByFn(_index: number, item: NTodo.TodoData){
+    return item.id;
+  }
 }
--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NTodo } from '../../models/todo.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent {
+  @Input({required: true}) todoData!: NTodo.TodoData;
+  @Input() first!: boolean;
+  @Input() last!: boolean;
+  @Input() odd!: boolean;
+  @Input() even!: boolean;
+  @Output() onClickIcon = new EventEmitter<NTodo.TodoData>();
 
+  get priority(): string {
+    switch (this.todoData.priority) {
+      case NTodo.Priority.LOW:
+        return NTodo.PriorityText.LOW;
+      case NTodo.Priority.MEDIUM: 
+        return NTodo.PriorityText.MEDIUM;
+      default:
+        return NTodo.PriorityText.HIGH;
+    }
+  }
 }
